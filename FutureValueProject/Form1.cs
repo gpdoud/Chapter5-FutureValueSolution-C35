57a1a7e17b833175b39c921d430104ed842c1227
Greg Doud <gdoud@gmail.com>
2022-01-30 13:20:06 -0500

    Add project files.

---
+++ FutureValueProject/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FutureValueProject {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e) {
+            decimal monthlyInvestment = Convert.ToDecimal(txtMonthlyInvestment.Text);
+            decimal yearlyInterestRate = Convert.ToDecimal(txtInterestRate.Text);
+            int years = Convert.ToInt32(txtYears.Text);
+
+            int months = years * 12;
+            decimal monthlyInterestRate = yearlyInterestRate / 12 / 100;
+
+            decimal futureValue = 0m;
+            for(int i = 0; i < months; i++) {
+                futureValue = (futureValue + monthlyInvestment) * (1 + monthlyInterestRate);
+            }
+
+            txtFutureValue.Text = futureValue.ToString("c");
+            txtMonthlyInvestment.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+    }
+}
